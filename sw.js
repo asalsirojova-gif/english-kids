4c89271a7f6bf9532d196648e14ec3601ac29dfb
asalsirojova-gif <asalsirojova@gmail.com>
2026-09-02 18:03:07 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,8 +36,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
